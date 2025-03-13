7d389745a96a75530092e6d390541f2678d9d31f
Erik230303 <23030313@itesa.edu.mx>
2025-03-13 09:42:36 -0600

    Primer vez

---
+++ src/main/java/erik/metodosnumericos/MetodosNumericos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package erik.metodosnumericos;
+
+/**
+ *
+ * @author Erik Vera
+ */
+public class MetodosNumericos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
